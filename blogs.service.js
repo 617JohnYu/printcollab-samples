4bdc23c5ca2643091ba3e76d860efced356e9a39
John <617JohnYu@users.noreply.github.com>
2017-03-11 00:44:09 -0800

    updated

--- blogs.service.js
+++ blogs.service.js
@@ -1,4 +1,4 @@
- // Blogs Service that will have functions to Insert, get, etc the Blog document into db
+
  module.exports = blogsService
 
  function blogsService(options) {
@@ -21,43 +21,36 @@
      }
 
      function ping() {
-         return new Promise(function(resolve, reject) { // is a method returns a promise.
+         return new Promise(function(resolve, reject) {
              resolve('pong from blogs.services')
          })
      }
-     // this sets up a default query conditon alloqing me to pass in querycondition or if not, find all blogs
-     function getAll(queryCondition = {}) { // is a method returns a promise. same as calling db.blogs.find() in mongo shell
+ 
+     function getAll(queryCondition = {}) {
          return Blog.find(queryCondition)
      }
 
-     function getOne(queryCondition) { // is a method returns a promise.
+     function getOne(queryCondition) {
          return Blog.findOne(queryCondition)
      }
 
-     function getBlogByTagId(queryCondition) { // is a method returns a promise.
+     function getBlogByTagId(queryCondition) {
          console.log(queryCondition)
          return Blog.find(queryCondition)
      }
 
-     function insert(document) { // is a method returns a promise.
+     function insert(document) {
          let blog = new Blog(document)
          return blog.save()
      }
 
-     function updateOne(queryCondition, doc) { // is a method returns a promise.
+     function updateOne(queryCondition, doc) {
          return Blog.findOneAndUpdate(queryCondition, doc, {
              new: true
          })
      }
 
-     function removeOne(queryCondition) { // is a method returns a promise.
+     function removeOne(queryCondition) {
          return Blog.findOneAndRemove(queryCondition)
      }
  }
-
- // notes:
- // Define any required fiellds in the scehma.
- // create functions in the service to perform crud operations to connect with db
- // controller acts as the enterypoint for the request, it calls the service and handles the promise that is returned
- // blogs route will create an instance of the router and direct requests to api to correct controller fucntions
- // Pipeline for request should be routes file -> controller -> service -> db
